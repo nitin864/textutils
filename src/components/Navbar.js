eaa03f333616ef86f7d45e1f2dafca28e3fe7aeb
NITIN <91000126+nitin864@users.noreply.github.com>
2024-11-11 10:10:21 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+             Textutils by Nitin
+            </a>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+ 
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
